98336617390e2eab02ca651da4d29ed795e06c44
Sumit Shrestha <sumitshrestha@Sumits-Air.attlocal.net>
2024-06-25 14:48:32 -0700

    Updating files

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const box1 = document.getElementById("box1");
+
+    box1.addEventListener("click", function() {
+        window.open("https://www/popeyes.com", "_blank");
+    })
+})
